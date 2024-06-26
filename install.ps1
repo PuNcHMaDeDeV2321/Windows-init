536e40e7166622a4010102f89f78c641c5573ce0
PuNcHMaDeDeV2321 <160447914+PuNcHMaDeDeV2321@users.noreply.github.com>
2024-06-26 16:00:36 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,16 @@
+# Función para imprimir mensajes de éxito en verde
 function Write-Success {
     param([string]$message)
     Write-Host $message -ForegroundColor Green
 }
 
+# Función para imprimir mensajes de error en rojo
 function Write-Error {
     param([string]$message)
     Write-Host $message -ForegroundColor Red
 }
 
+# Función para instalar programas usando winget
 function Install-Winget {
     param (
         [string]$packageName,
@@ -15,7 +18,7 @@ function Install-Winget {
     )
     try {
         Write-Host "Instalando $displayName..."
-        winget install $packageName -s winget -e
+        winget install --id $packageName -e
         Write-Success "$displayName instalado correctamente."
     } catch {
         Write-Error "Error al instalar $displayName con winget: $_"
@@ -25,7 +28,8 @@ function Install-Winget {
 Install-Winget -packageName "Microsoft.PowerToys" -displayName "Microsoft PowerToys"
 Install-Winget -packageName "Microsoft.VisualStudioCode" -displayName "Visual Studio Code"
 Install-Winget -packageName "Python.Python.3" -displayName "Python"
-Install-Winget -packageName "BraveSoftware.Brave-Browser" -displayName "Brave Browser"
 Install-Winget -packageName "NodeJS.NodeJS" -displayName "Node.js"
+Install-Winget -packageName "Brave.Brave" -displayName "Brave Browser"
 
 Write-Host "Instalación de programas completada."
+
